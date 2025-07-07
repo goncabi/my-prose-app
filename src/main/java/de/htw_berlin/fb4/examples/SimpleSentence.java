c71351d7157c312e9931b30d26b2600c63da21ab
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-07-07 11:53:03 +0200

    feat: add constructor to SimpleSentence that accepts a String

--- src/main/java/de/htw_berlin/fb4/examples/SimpleSentence.java
+++ src/main/java/de/htw_berlin/fb4/examples/SimpleSentence.java
@@ -48,6 +48,23 @@ public class SimpleSentence implements Sentence {
      * @throws NullPointerException if the provided Prose object is null
      * @see de.htw_berlin.fb4.ossd.prose.Prose#get()
      */
+
+public SimpleSentence() {
+// Uses the default value already set in the field
+} /**
+     * Constructs a SimpleSentence with the given text.
+     *
+     * @param text The sentence text to be used
+     */
+ public SimpleSentence(String text) {
+        this.text = text;
+    }
+
+    /**
+     * Converts a Prose object into a Sentence object.
+     * ...
+     */
+
     public static Sentence toSentence(Prose prose) {
         SimpleSentence sentence = new SimpleSentence();
         sentence.text = prose.get();
